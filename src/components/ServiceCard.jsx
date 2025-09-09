f3b30256d3dfd565057aa1e087fdc2c9d2982a7e
Kamtimacjc025 <wethinkcode@pop-os.localdomain>
2025-09-09 11:36:30 +0200

    serviceCard

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import Card from './ui/Card';
 
-export default function ServiceCard() {
+export default function ServiceCard({ title, description, icon }) {
   return (
-    <div>ServiceCard</div>
-  )
+    <Card className="p-6 text-center">
+      <img src={icon} alt={title} className="w-12 h-12 mx-auto mb-4" />
+      <h3 className="text-xl font-semibold mb-2">{title}</h3>
+      <p className="text-secondary-2">{description}</p>
+    </Card>
+  );
 }
